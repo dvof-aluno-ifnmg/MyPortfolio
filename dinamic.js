0fd2e02b5fdaa39e7fa2ac85f36c57819253539b
dvof-aluno-ifnmg <dvof@aluno.ifnmg.edu.br>
2025-11-12 13:03:06 -0300

    improve dynamism

---
+++ dinamic.js
@@ -0,0 +1,18 @@
+//declaracao de objetos 
+const link_linkedin_obj = document.querySelectorAll(".link_linkedin")
+
+
+
+
+
+//declaracao de links de variáveis 
+const link_linkedin = "https://br.linkedin.com/"
+
+
+
+
+
+//definicao de valores
+for(const i of link_linkedin_obj){ 
+i.href = link_linkedin
+}
